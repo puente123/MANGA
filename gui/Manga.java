c5429a0fbc73c332a949f96eb994cd86c8aef55a
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-04-05 20:01:47 -0500

    edited manga

--- gui/Manga.java
+++ gui/Manga.java
@@ -16,22 +16,3 @@ public class Manga {
         
     }
 }
-
-package gui;
-
-public class Manga {
-    
-    public static void main(String[] args){
-        
-        String storeName = null;
-        if(args.length > 0){
-            storeName = args[0];
-        }
-        else{
-            storeName = "Nameless";
-        }
-
-        new MainWin(storeName);
-        
-    }
-}
